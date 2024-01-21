9cefd905240c3e142f8b2a81d55909fa17e6b7f7
ITRDC <shedu.team@gmail.com>
2024-01-21 13:36:30 +0300

    adding app.py

--- app.py
+++ app.py
@@ -1,14 +1,21 @@
-n = int( input("Enter A Number: "))
+import pandas as pd
 
-if n > 0 :
-    print('the Number was Positive')
-elif n < 0:
-    print('the Number was Nigative')
-else:
-    print('the Number is Zero')
+d = {'col1': [1, 2, 3, 4, 7],
+     'col2': [4, 5, 6, 9, 5],
+     'col3': [8, 8, 7, 2, 5],
+     'col4': [4, 5, 6, 7, 5],
+     'col5': [8, 5, 3, 9, 5],
+     'col6': [4, 5, 3, 9, 5],
+     'col7': [4, 6, 5, 9, 5],
+     'col8': [7, 8, 12, 1, 11]
+     }
+df = pd.DataFrame(data=d)
+
+print(df)
+
+# this function is returning the number of rows.
+print(f"No of Rows = {df.shape[0]}. and No of columns = {df.shape[1]}.")
 
 
-
-#name = input("Please Enter Your Name: ")
-#print (f"Hello, {name}")
-
+pulseMax = max(80, 50, 82, 95)
+print(pulseMax)
